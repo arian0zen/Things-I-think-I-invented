b5d4a7d4c554c6ba6ee3d44032bd105fd5181e81
Arian Shaikh <68517592+arian0zen@users.noreply.github.com>
2022-09-30 18:35:55 +0530

    Update SortingAnArray_in_least_time.py

--- SortingAnArray_in_least_time.py
+++ SortingAnArray_in_least_time.py
@@ -9,8 +9,11 @@ length = len(arr)
 heapq.heapify(heapArray)
 sortedArray = []
 
+
 for each in (arr[:length]):
     sortedArray.append(heapq.heappop(heapArray))
     
 arr = sortedArray
 print ("The sorted array is:", arr)
+
+
